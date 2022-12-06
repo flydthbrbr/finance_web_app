5b028ced1e690d46d4301c8d67366826bc9f9205
flydthbrbr <33043010+flydthbrbr@users.noreply.github.com>
2022-12-06 17:46:37 +0000

    Add files via upload

---
+++ finance_web_app/website/helpers.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+
+def get_ticker_data(ticker):
+    stock = yf.Ticker(ticker)
+    return stock
---
+++ finance_web_app/website/views.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import User, Note
+from . import helpers
+from . import db
+import json
+
+
+views = Blueprint('views', __name__)
+    
+@views.route('/',methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Great Success!', category='succes')
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/stocks', methods=['GET','POST'])
+@login_required
+def stocks():
+    stock = helpers.get_ticker_data('AAPL')
+    if request.method == 'POST':
+        ticker = request.form.get('ticker')
+        stock = helpers.get_ticker_data(ticker)
+        data = request.form 
+        print(data)
+        print(stock.info['longBusinessSummary'])
+        #return render_template("stocks.html", user=current_user, stock=stock, data=data)
+    return render_template("stocks.html", user=current_user, stock=stock)
+
+@views.route('/delete-note', methods=['POST']) 
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    return jsonify({})
+
